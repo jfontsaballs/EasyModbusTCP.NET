664ac22ce70188baa16069d93c1ca8d9676513cc
Jordi Font <jfont@teg.es>
2020-02-04 17:38:26 +0100

    Improve ServerApplication

--- ServerApplication/Program.cs
+++ ServerApplication/Program.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using EasyModbus;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -9,27 +10,84 @@ namespace ServerApplication
 
     class Program
     {
+        private ModbusServer modbusServer;
+
         static void Main(string[] args)
         {
             Program application = new Program();
             application.startServer();
-
         }
 
         public void startServer()
         {
-            EasyModbus.ModbusServer modbusServer = new EasyModbus.ModbusServer();
+            modbusServer = new ModbusServer();
+            modbusServer.UnitIdentifier = 255;
+
+            modbusServer.holdingRegisters[1012] = 1100;
+            modbusServer.holdingRegisters[1022] = 1200;
+            modbusServer.holdingRegisters[1032] = 1300;
+            modbusServer.holdingRegisters[1042] = 1400;
+            modbusServer.holdingRegisters[1052] = 1500;
+
+            modbusServer.HoldingRegistersChanged += holdingRegistersChanged;
+            modbusServer.NumberOfConnectedClientsChanged += ModbusServer_NumberOfConnectedClientsChanged;
+
             modbusServer.Listen();
-            modbusServer.HoldingRegistersChanged += new EasyModbus.ModbusServer.HoldingRegistersChangedHandler(holdingRegistersChanged);
-            Console.ReadKey();
+
+            Console.WriteLine("ModbusTCP server started");
+
+            while (true)
+            {
+                try
+                {
+                    var read = Console.ReadLine().Trim();
+                    if (read.Contains(" "))
+                    {
+                        var addressAndValue = read.Split(' ');
+                        var address = int.Parse(addressAndValue[0]);
+                        var value = short.Parse(addressAndValue[1]);
+                        modbusServer.holdingRegisters[address] = value;
+                    }
+                    else if (read == "q4" || read == "p4")
+                    {
+                        for (int i = 0; i < modbusServer.holdingRegisters.localArray.Length; i++)
+                            if (modbusServer.holdingRegisters[i] != 0)
+                                PrintHoldingRegister(i);
+                    }
+                    else if (read == "exit" || read == "quit")
+                        break;
+                }
+                catch(Exception ex)
+                {
+                    Console.WriteLine(ex.Message);
+                }
+            }
             
             modbusServer.StopListening();
         }
 
+        private int previousNumberOfConnections = -1;
+        private void ModbusServer_NumberOfConnectedClientsChanged()
+        {
+            if (modbusServer.NumberOfConnections != previousNumberOfConnections)
+            {
+                previousNumberOfConnections = modbusServer.NumberOfConnections;
+                Console.WriteLine($"Connected clients: {modbusServer.NumberOfConnections}");
+            }
+        }
+
         public void holdingRegistersChanged(int startingAddress, int quantity)
         {
-            Console.WriteLine(startingAddress);
-            Console.WriteLine(quantity);
+            var endAddress = startingAddress + quantity - 1;
+            if (quantity == 1)
+                Console.WriteLine($"Changed {startingAddress}");
+            else
+                Console.WriteLine($"Changed {startingAddress} to {endAddress}");
+
+            for(int i = startingAddress; i <= endAddress; i++)
+                PrintHoldingRegister(i);
         }
+
+        private void PrintHoldingRegister(int i) => Console.WriteLine($"    {400_000 + i} = {modbusServer.holdingRegisters[i]}");
     }
 }
